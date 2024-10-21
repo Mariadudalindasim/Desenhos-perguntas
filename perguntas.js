0fb8372b7286de9c5f6ab4bfb302634b5f96586f
Mariadudalindasim <169056238+Mariadudalindasim@users.noreply.github.com>
2024-10-21 09:54:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Monster High',
+    'Qual o nome do namorado da Lagoona?',
+    'O nome dele é Gil'
+)
+
+criaCartao(
+    'Winx',
+    'Qual é o nome da ex do Sky?',
+    'Princesa Diaspro '
+)
+
+criaCartao(
+    'My Little Pony ',
+    'Qual o nome da magia delas?',
+    'Magia da Amizade'
+)
+
+criaCartao(
+    'Barbie',
+    'Qual é o nome da casa da barbir?',
+    'Dreamhouse'
+)
